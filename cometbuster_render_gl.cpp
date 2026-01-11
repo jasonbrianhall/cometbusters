c560a845e31654da78e144bb0a354fd2c7ff9ad3
Jason Hall <jasonbrianhall@gmail.com>
2026-01-10 23:30:52 -0500

    Latest

--- cometbuster_render_gl.cpp
+++ cometbuster_render_gl.cpp
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
-#include <pango/pango.h>
+//#include <pango/pango.h>
 #include <gtk/gtk.h>
 #include "cometbuster.h"
 #include "visualization.h"
